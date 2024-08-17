290507d2ec2443bc1a1455cf6e1880fd618abf1a
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-08-17 15:51:08 +0530

    fix: hydration wrror

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import CreateWallet from "@/components/CreateWallet";
+import { useEffect, useState } from "react";
 import useLocalStorage from "use-local-storage";
 
 
 export default function Home() {
 
+  const [isClient, setIsClient] = useState(false);
   const [mnemonic, setMnemonic] = useLocalStorage<string>('mnemonic', '');
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
   return (
 
     mnemonic === '' ?
